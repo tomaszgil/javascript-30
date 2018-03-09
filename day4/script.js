d0eae98e16d84faf5e1732c5363af4ff7e52c3bc
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-03-09 11:42:52 +0100

    Implement day 4 task

---
+++ day4/script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+class App {
+  constructor() {
+    this.workout = new Workout();
+  }
+
+  run() {
+    this.workout.do();
+  }
+}
+
+class Workout {
+  do() {
+    const inventors = [
+      { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+      { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+      { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+      { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+      { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+      { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+      { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+      { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+      { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+      { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+      { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+      { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+    ];
+    const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+    // Array.prototype.filter()
+    // 1. Filter the list of inventors for those who were born in the 1500's
+    const filteredInventors = inventors.filter(i => i.year >= 1500 && i.year < 1600);
+    console.log(filteredInventors);
+
+    // Array.prototype.map()
+    // 2. Give us an array of the inventors' first and last names
+    const fullNames = inventors.map(i => `${i.first} ${i.last}`);
+    console.log(fullNames);
+
+    // Array.prototype.sort()
+    // 3. Sort the inventors by birthdate, oldest to youngest
+    const sorted = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+    console.log(sorted);
+
+    // Array.prototype.reduce()
+    // 4. How many years did all the inventors live?
+    const yearSum = inventors.reduce((total, i) => total += (i.passed - i.year), 0);
+    console.log(yearSum);
+
+    // 5. Sort the inventors by years lived
+    const oldest = inventors.sort((a, b) => {
+      return (a.passed - a.year) < (b.passed - b.year) ? 1 : -1;
+    });
+    console.log([...oldest]); // prevent overwriting
+
+    // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+    // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+    const links = document.querySelectorAll('.mw-category a');
+    const names = Array.from(links).map(a => a.innerHTML);
+    const onlyDe = names.filter(name => /de/.test(name));
+
+    // 7. sort Exercise
+    // Sort the people alphabetically by last name
+    const lastNameSorted = people.sort((a, b) => {
+      const [aLast, aFirst] = a.split(', ');
+      const [bLast, bFirst] = b.split(', ');
+
+      return aLast > bLast ? 1 : -1
+    });
+    console.log(lastNameSorted);
+
+    // 8. Reduce Exercise
+    // Sum up the instances of each of these
+    const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+    const instances = data.reduce((obj, el) => {
+      if (!obj[el]) {
+        obj[el] = 0;
+      }
+      obj[el]++;
+      
+      return obj;
+    }, {});
+    console.log(instances);
+  }
+}
+
+window.onload = () => {
+  const app = new App();
+  app.run();
+};
